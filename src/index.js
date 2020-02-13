f350e3c0e2b22c01aa09cb8d034b0baa0c0cf7b0
Josh Menden <josh.menden@gmail.com>
2020-02-12 18:37:43 -0700

    Basic creation is complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
+var AWS = require('aws-sdk');
+
 exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from my NPM script'),
+  var params = {
+    Document: {
+      S3Object: {
+        Bucket: 'bartleby',
+        Name: 'fake-w2-forms.jpg',
+      }
+    },
+    FeatureTypes: ["FORMS"]
   };
-  return response;
+
+  var textract = new AWS.Textract();
+
+  const textractPromise = new Promise(function (resolve, reject) {
+    textract.analyzeDocument(params, function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  })
+
+  return textractPromise
+  // textractPromise.then(res => {
+  //   const response = {
+  //     statusCode: 200,
+  //     textract: JSON.stringify(res),
+  //     body: JSON.stringify('Hello THIS IS US from my NPM script'),
+  //   };
+
+  //   return response;
+  // })
 };
